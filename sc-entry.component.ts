82dc568814a793b1d4fd76617ae3e7aa3adbfa21
RAMESH AITHAGOINA <88446859+Ramesh615@users.noreply.github.com>
2022-01-01 00:55:24 +0530

    Add files via upload

---
+++ sc-entry.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-sc-entry',
+  templateUrl: './sc-entry.component.html',
+  styleUrls: ['./sc-entry.component.scss']
+})
+export class ScEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
